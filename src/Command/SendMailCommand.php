7f993e116b199b995c665b2368fb72b23304b043
unknown <anjuere.corentin@gmail.com>
2018-03-22 12:01:04 +0100

    envoi du mail via MailDev si event demain

--- src/Command/SendMailCommand.php
+++ src/Command/SendMailCommand.php
@@ -7,6 +7,9 @@ use App\Entity\Weekend;
 use App\Repository\ExhibitRepository;
 use App\Repository\PartyRepository;
 use App\Repository\WeekendRepository;
+use Swift_Mailer;
+use Swift_Message;
+use Swift_SmtpTransport;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -16,12 +19,14 @@ class SendMailCommand extends Command
     private $weekendRepository;
     private $exhibitRepository;
     private $partyRepository;
+    private $mailer;
 
-    public function __construct(WeekendRepository $weekendRepository, ExhibitRepository $exhibitRepository, PartyRepository $partyRepository)
+    public function __construct(WeekendRepository $weekendRepository, ExhibitRepository $exhibitRepository, PartyRepository $partyRepository, Swift_Mailer $mailer)
     {
         $this->weekendRepository = $weekendRepository;
         $this->exhibitRepository = $exhibitRepository;
         $this->partyRepository = $partyRepository;
+        $this->mailer = $mailer;
 
         parent::__construct();
     }
@@ -46,6 +51,7 @@ class SendMailCommand extends Command
         foreach ($events as $event) {
             // wait for users/events relations to get attendees
 
+
             $mailBody = '';
 
             if ($event instanceof Weekend) {
@@ -67,7 +73,11 @@ class SendMailCommand extends Command
                 $output->writeln('Event type is unknown');
             }
 
-            dump($mailBody);die;
-        }
+            $message = (new Swift_Message('Wonderful Subject'))
+               ->setFrom(['chevalproject@gmail.com' => 'Projet Cheval'])
+               ->setTo(['anjuere.corentin@gmail.com'])
+               ->setBody($mailBody);
+            $r = $this->mailer->send($message);
+            }
     }
 }
