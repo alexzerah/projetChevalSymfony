44bad7e568353b921e347488551d5560461d712d
Charlotte <chpalma97@gmail.com>
2018-03-23 14:13:41 +0100

    reset passwork work in progress

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,14 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Entity\User;
+use App\Form\ForgotPassword;
 use App\Form\PassChangeFormType;
 use App\Form\PostType;
 use App\Repository\FetchRepository;
 use App\Services\Calculator;
+use Swift_Mailer;
+use Swift_Message;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\RouterInterface;
 
 class MainController extends Controller
 {
@@ -53,23 +58,51 @@ class MainController extends Controller
     /**
      * @Route("/mot-de-passe-oublie", name="forgotpassword")
      */
-    public function forgotpassword()
+    public function forgotpassword(Request $request, Swift_Mailer $mailer, RouterInterface $router)
     {
+
+        $form = $this->createForm(ForgotPassword::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $email = $form->get('email')->getData();
+            $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['email' => $email]);
+
+            if(!$user){
+                return $this->render('site/forgotpassword.html.twig', [
+                    'form' => $form->createView(),
+                    'invalid_email' => $email,
+                ]);
+            }
+
+            $token = uniqid('bde-', true);
+
+            $url = $router->generate('changepassword', [
+                'token' => $token,
+            ]);
+            $mailBody =  'Pour reset ton password click ici : ' . $url;
+
+            $message = (new Swift_Message('Nouveau mot de passe'))
+                ->setFrom(['chevalproject@gmail.com' => 'Projet Cheval'])
+                ->setTo('charlotte.palma@teching.com')
+                ->setBody($mailBody);
+            $mailer->send($message);
+
+
+
+        }
+
+
+
         return $this->render('site/forgotpassword.html.twig', [
+            'form' => $form->createView(),
         ]);
+
     }
 
-    /**
-     * @Route("/404", name="quatrecentquatre")
-     */
-    public function quatrecentquatre()
-    {
-        return $this->render('site/error404.html.twig', [
-        ]);
-    }
 
     /**
-     * @Route("/changer-mon-mot-de-passe", name="changepassword")
+     * @Route("/changer-mon-mot-de-passe/{token}", name="changepassword")
      */
     public function changepassword(Request $request)
     {
@@ -85,4 +118,22 @@ class MainController extends Controller
             'form' => $form->createView(),
         ]);
     }
+
+
+
+
+
+
+
+
+    /**
+     * @Route("/404", name="quatrecentquatre")
+     */
+    public function quatrecentquatre()
+    {
+        return $this->render('site/error404.html.twig', [
+        ]);
+    }
+
+
 }
